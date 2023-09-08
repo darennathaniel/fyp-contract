fe22294e430d6c20fe41f800ae5050a8cd01da9d
Daren Nathaniel Janto <67410689+darennathaniel@users.noreply.github.com>
2023-09-08 20:54:58 +0800

    created a role contract

--- contracts/Owner.sol
+++ contracts/Owner.sol
@@ -2,29 +2,29 @@
 pragma solidity 0.8.19;
 
 contract Owner {
-  address public owner;
+    address public owner;
 
-  event TransferOwnership(address currentOwner, address newOwner);
+    event TransferOwnership(address currentOwner, address newOwner);
 
-  constructor() {
-      owner = msg.sender;
-      emit TransferOwnership(address(0), owner);
-  }
+    constructor() {
+        owner = msg.sender;
+        emit TransferOwnership(address(0), owner);
+    }
 
-  modifier onlyOwner() {
-    require(
-      msg.sender == owner,
-      "This function is restricted to the contract's owner"
-    );
-    _;
-  }
+    modifier onlyOwner() {
+        require(
+        msg.sender == owner,
+        "This function is restricted to the contract's owner"
+        );
+        _;
+    }
 
-  function isOwner(address checkOwner) public view returns (bool) {
-      return owner == checkOwner;
-  }
+    function isOwner(address checkOwner) public view returns (bool) {
+        return owner == checkOwner;
+    }
 
-  function transferOwnership(address newOwner) public onlyOwner {
-      emit TransferOwnership(owner, newOwner);
-      owner = newOwner;
-  }
+    function transferOwnership(address newOwner) public onlyOwner {
+        emit TransferOwnership(owner, newOwner);
+        owner = newOwner;
+    }
 }
--- contracts/roles/Role.sol
+++ contracts/roles/Role.sol
@@ -1,8 +1,15 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.19;
 
-contract Product {
-    address public productId;
+contract Role {
+    enum RoleType {
+        Customer,
+        Distributor,
+        Supplier,
+        Manufacturer,
+        Retailer
+    }
+    address public id;
     RoleType public roleType;
     event Transcation(address id, RoleType roleType, string message, uint256 timestamp);
 
